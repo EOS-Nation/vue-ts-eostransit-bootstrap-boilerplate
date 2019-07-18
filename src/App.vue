0da51e67ad652583afc3fe57d18f2c3877e3f3ce
Jan Langheimer <langheimer@gmail.com>
2019-07-18 17:19:50 +0200

    add modal login component

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,20 @@
         <router-view />
       </b-col>
     </b-row>
+    <modal-login />
   </b-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import Navigation from '@/components/layout/Navigation.vue'
+import ModalLogin from '@/components/authentication/ModalLogin.vue'
 import { vxm } from '@/store/'
 
 @Component({
   components: {
-    Navigation
+    Navigation,
+    ModalLogin
   }
 })
 export default class App extends Vue {
