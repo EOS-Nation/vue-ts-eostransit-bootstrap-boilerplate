560b0e5d9f837c55d62712c11b990f6b320d672c
Jan Langheimer <langheimer@gmail.com>
2019-09-23 15:08:20 +0200

    add voters table

--- src/views/Voters.vue
+++ src/views/Voters.vue
@@ -1,5 +1,43 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <b-table
+      striped
+      hover
+      :fields="fields"
+      :items="voters"
+      :sort-by.sync="sortBy"
+      :sort-desc.sync="sortDesc"
+    >
+      <!-- A virtual column -->
+      <template v-slot:cell(index)="data">
+        {{ data.index + 1 }}
+      </template>
+    </b-table>
   </div>
 </template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { vxm } from '@/store'
+
+@Component
+export default class Voters extends Vue {
+  fields = [
+    { key: 'index', label: '#', sortable: false },
+    { key: 'owner', sortable: true },
+    { key: 'proxy', sortable: false },
+    { key: 'producers', sortable: false },
+    { key: 'staked', sortable: true },
+    { key: 'last_vote_weight', sortable: true },
+    { key: 'is_proxy', sortable: true },
+    { key: 'next_claim_period', sortable: true }
+  ]
+  sortBy = 'owner'
+  sortDesc = false
+  get voters() {
+    return vxm.core.voters
+  }
+  created() {
+    vxm.core.getVoters()
+  }
+}
+</script>
