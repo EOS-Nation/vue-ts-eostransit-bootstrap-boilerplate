9e12be85257b68efe28709e54c50537807a6738f
Jan Langheimer <langheimer@gmail.com>
2019-09-23 12:47:09 +0200

    rework step 2

--- src/components/steps/Step2.vue
+++ src/components/steps/Step2.vue
@@ -1,55 +1,56 @@
 <template>
-  <div>
-    <h2>Step 2</h2>
-    <h3>Signup</h3>
-    <p v-if="userSigned">done</p>
-    <b-btn v-else @click="signup()" variant="primary">Signup Now</b-btn>
-  </div>
+  <step-ui :step="2" title="Signup">
+    <template v-if="userSigned">
+      <p>done</p>
+    </template>
+    <template v-else>
+      <p>Please signup first before you can claim your rewards.</p>
+      <b-btn @click="signup()" variant="primary">
+        <font-awesome-icon
+          v-if="!loading"
+          icon="user-plus"
+          fixed-width
+          class="mr-1"
+        />
+        <font-awesome-icon
+          v-else
+          icon="spinner"
+          spin
+          fixed-width
+          class="mr-1"
+        />
+        Signup Now
+      </b-btn>
+    </template>
+  </step-ui>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator'
 import { vxm } from '@/store/'
 import { VotersTable } from '@/types/proxy'
-
-@Component
+import StepUi from '@/components/steps/StepUi.vue'
+@Component({
+  components: { StepUi }
+})
 export default class Step2 extends Vue {
-  @Prop() private msg!: string
-
-  get voters() {
-    return vxm.core.voters
-  }
+  loading = false
+  signupResp: any = false
 
   get isAuthenticated(): string | false {
     return vxm.eosTransit.isAuthenticated
   }
 
   get userSigned() {
-    return vxm.core.userSigned
+    return vxm.core.userSignedUp
   }
 
-  get language() {
-    return vxm.core.language
-  }
-
-  set language(lang: string) {
-    vxm.core.setLanguage(lang)
-  }
-
-  signup() {
-    vxm.core.signup()
+  async signup() {
+    this.loading = true
+    this.signupResp = await vxm.core.signup()
+    this.loading = false
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 10px;
-}
-</style>
+<style scoped lang="scss"></style>
