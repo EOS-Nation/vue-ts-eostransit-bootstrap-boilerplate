4d22c1919ae390422735614dd2e1af714f3bb9fa
Jan Langheimer <langheimer@gmail.com>
2019-09-23 11:05:22 +0200

    add step 3 component

---
+++ src/components/steps/Step3.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h2>Step 3</h2>
+    <h3>Claim</h3>
+    <b-btn @click="claim()" variant="primary">CLAIM</b-btn>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import { vxm } from '@/store/'
+
+@Component
+export default class Step3 extends Vue {
+  @Prop() private msg!: string
+
+  get userSigned() {
+    return vxm.core.userSigned
+  }
+
+  get language() {
+    return vxm.core.language
+  }
+
+  set language(lang: string) {
+    vxm.core.setLanguage(lang)
+  }
+
+  claim() {
+    vxm.core.claim()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 10px;
+}
+</style>
