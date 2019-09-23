90c2d9adda30d74a8a02a8a24f095125d6227fd4
Jan Langheimer <langheimer@gmail.com>
2019-09-23 11:05:33 +0200

    add steps ui component

---
+++ src/components/steps/StepUi.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h2>Step 1</h2>
+    <h3>Proxy</h3>
+    <p>Vote for our Proxy: PROXY4NATION</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import { vxm } from '@/store/'
+
+@Component
+export default class Step1 extends Vue {
+  @Prop() private msg!: string
+
+  get language() {
+    return vxm.core.language
+  }
+
+  set language(lang: string) {
+    vxm.core.setLanguage(lang)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 10px;
+}
+</style>
