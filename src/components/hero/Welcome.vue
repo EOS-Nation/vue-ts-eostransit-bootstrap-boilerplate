26611f3935c139b3af12e8804b59fb152ff4222e
Jan Langheimer <langheimer@gmail.com>
2019-09-25 22:56:18 +0200

    rework welcome view

--- src/components/hero/Welcome.vue
+++ src/components/hero/Welcome.vue
@@ -1,39 +1,25 @@
 <template>
-  <hero-ui
-    title="Vote for our proxy and claim your share 4% APR every 24 hours!"
-  >
-    <template v-slot:headline>
-      <h1 class="display-4 font-w700 text-white mb-3">
-        PROXY<span class="text-primary-light">4</span>NATION
-      </h1>
-    </template>
-    <template v-slot:button>
-      <b-btn @click="login()" variant="primary" :disabled="loading">
-        <font-awesome-icon
-          v-if="!loading"
-          icon="arrow-right"
-          fixed-width
-          class="mr-1"
-        />
-        <font-awesome-icon
-          v-else
-          icon="spinner"
-          spin
-          fixed-width
-          class="mr-1"
-        />
-        Get Started
-      </b-btn>
-    </template>
-  </hero-ui>
+  <div>
+    <rewards class="my-5" />
+    <b-btn @click="login()" variant="hero-primary" :disabled="loading" class="mt-2">
+      <font-awesome-icon
+        v-if="!loading"
+        icon="arrow-right"
+        fixed-width
+        class="mr-1"
+      />
+      <font-awesome-icon v-else icon="spinner" spin fixed-width class="mr-1" />
+      Get Started
+    </b-btn>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator'
 import { vxm } from '@/store/'
-import HeroUi from '@/components/hero/HeroUi.vue'
+import Rewards from '@/components/layout/Rewards.vue'
 @Component({
-  components: { HeroUi }
+  components: { Rewards }
 })
 export default class Welcome extends Vue {
   voteResp: any = false
