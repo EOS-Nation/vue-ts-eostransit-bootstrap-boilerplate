9e8fe9ebbfc2851d406b7e010371f8d083cb3696
Jan Langheimer <langheimer@gmail.com>
2019-09-26 22:38:39 +0200

    fix interval calculation

--- src/components/layout/ProxyNation.vue
+++ src/components/layout/ProxyNation.vue
@@ -3,9 +3,9 @@
     <h1 class="display-4 font-w700 text-white mb-3">
       PROXY<span class="text-primary-light">4</span>NATION
     </h1>
-    <h2 v-if="apr && period" class="font-w300 text-white-75 mb-2">
-      Vote for our proxy and claim your share {{ apr }}% APR every
-      {{ period }} hours!
+    <h2 v-if="rate && interval" class="font-w300 text-white-75 mb-2">
+      Vote for our proxy and claim your share {{ rate }}% APR every
+      {{ interval }}!
     </h2>
     <h2 v-else class="font-w300 text-white-75 mb-2">
       Vote for our proxy and claim your share
@@ -21,18 +21,18 @@ import { vxm } from '@/store/'
 
 @Component
 export default class ProxyNation extends Vue {
-  apr: number | false = false
-  period: number | false = false
-
-  async created() {
-    const settings = await vxm.eosTransit.accessContext.eosRpc.get_table_rows({
-      code: 'proxy4nation',
-      table: 'settings',
-      scope: 'proxy4nation',
-      limit: 1
-    })
-    this.apr = settings.rows[0].rate / 100
-    this.period = settings.rows[0].interval / 60 / 60
+  get rate() {
+    const rate = vxm.core.settings
+    if (rate && rate.rate) return rate.rate / 100
+    else return false
+  }
+  get interval() {
+    const interval = vxm.core.settings
+    if (interval && interval.interval) {
+      let hours = interval.interval / 60 / 60
+      if (hours >= 1) return hours + ' hours'
+      else return hours * 60 + ' minutes'
+    } else return false
   }
 }
 </script>
